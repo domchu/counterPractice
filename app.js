c892fecd02556fe6cedc02f0c6472ee0a43aa8a3
Mr.Dominic <ochu.dominic.onah@gmail.com>
2024-05-10 12:06:31 +0100

    done with the counter

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const state = {
   count: 0,
 };
+
 const handleIncrement = () => {
   state.count++;
 };
-
+const handleDecrement = () => {
+  state.count--;
+};
 const render = () => {
   document.querySelector("h1").innerHTML = state.count;
 };
@@ -13,5 +16,10 @@ const handlePlusBtnClick = () => {
   handleIncrement();
   render();
 };
+const handleMinusBtnClick = () => {
+  handleDecrement();
+  render();
+};
 
 document.querySelector("#plus").addEventListener("click", handlePlusBtnClick);
+document.querySelector("#minus").addEventListener("click", handleMinusBtnClick);
